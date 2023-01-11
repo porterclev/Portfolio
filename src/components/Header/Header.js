2de44a490aa6090c3a66056fbe5d769396eebe32
porterclev <porterclev@gmail.com>
2023-01-11 00:57:27 -0800

    fixed background image

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import Profile from "./assets/profile.jpg";
 const Header = () => {
   return (
     <section className="banner">
-      <img className="image" src={Banner} alt="banner image" />
+      <div className="header">Bruh</div>
 
       <div className="profile">
         <img className="profile-image" src={Profile} />
