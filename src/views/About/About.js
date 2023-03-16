83cda5a3a27461694308315ef803761580bd7b25
Porter Clevidence <porterclev@gmail.com>
2023-03-16 14:46:37 -0700

    moved profile picture to about section

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -1,12 +1,18 @@
 import "./About.css";
+import Profile from "../../assets/profile.jpg";
+
 const About = () => {
   return (
     <section className="about-container">
-      <h1>About Me</h1>
-      <p>
-        I'm a current student at California State University of Long Beach
-        working towards my bachalors in Computer Science.
-      </p>
+      <img className="profile-image" src={Profile} />
+
+      <div className="about-text-box">
+        <h1>About Me</h1>
+        <p>
+          I'm a current student at California State University of Long Beach
+          working towards my bachalors in Computer Science.
+        </p>
+      </div>
     </section>
   );
 };
